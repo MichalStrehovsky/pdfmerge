b7ed48cb9798b9900fe6529a7273b015694f5913
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2023-09-21 19:48:28 +0200

    First!

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+var outFileName = "out.pdf";
+var outputDocument = new PdfDocument();
+
+int mergedFiles = args.Length;
+for (int i = 0; i < args.Length; i++)
+{
+    if (args[i] == "--out")
+    {
+        if (++i == args.Length)
+            return ErrorExit("`--out` must be followed by the output file name");
+
+        outFileName = args[i++];
+        mergedFiles -= 2;
+        continue;
+    }
+
+    try
+    {
+        using PdfDocument doc = PdfReader.Open(args[i], PdfDocumentOpenMode.Import);
+
+        foreach (PdfPage page in doc.Pages)
+            outputDocument.AddPage(page);
+    }
+    catch (Exception ex)
+    {
+        return ErrorExit($"Failed processing {args[i]}: {ex.Message}");
+    }
+}
+
+if (mergedFiles < 2)
+    return ErrorExit("Must specify at least two files to merge");
+
+outputDocument.Save(outFileName);
+
+Console.WriteLine($"Merged PDF is available at {outFileName}");
+
+return 0;
+
+static int ErrorExit(string errorString)
+{
+    Console.WriteLine($"Error: {errorString}");
+    Console.WriteLine();
+    Console.WriteLine("Usage: pdfmerge file1.pdf file2.pdf [file3.pdf...] [--out output.pdf]");
+    Console.WriteLine("If --out is not specified, output is saved into out.pdf in the current directory.");
+    return 1;
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# pdfmerge
+
+Simple CLI tool to merge PDF files.
+
+This is a small demo of building CLI apps in .NET using [Native AOT](https://learn.microsoft.com/dotnet/core/deploying/native-aot/).
+
+Highlights:
+
+* Build small, selfcontained executables. The pdfmerge tool is 2 MB in size with _no dependencies_. Drop the executable on a new Linux/Windows/macOS machine with a clean OS install and it will just work. See the csproj for details on this configuration.
+* Use Github actions to manage your release: [CI pipeline](.github/workflows/ci.yml) set up to run on PRs but also manually-triggerable from the Actions tab in Github to help you create tagged releases with a couple clicks
